e594921c5d01e0707ecd341ffde2c82f34d90b34
Brehyma Traore <brehymatraore50@gmail.com>
2024-06-11 19:45:58 -0700

    Hot fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,11 +7,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height = 0;
+	size_t right_height = 0;
+
 	if (tree == NULL)
-		return (-1);
+		return (0);
 
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	return ((left_height > right_height ? left_height : right_height) + 1);
+	return ((left_height > right_height ? left_height : right_height));
 }
