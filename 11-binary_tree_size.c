619e2741b4f23e817107158d543ea18f65e268cc
Brehyma Traore <brehymatraore50@gmail.com>
2024-06-12 04:39:59 -0700

    Updated

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,12 +7,8 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t left_size = 0, right_size = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	left_size = binary_tree_size(tree->left);
-	right_size = binary_tree_size(tree->right);
-	return (left_size + right_size + 1);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
