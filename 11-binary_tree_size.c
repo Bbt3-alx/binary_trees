89ec3f430c80ce86fd13123b9f5828783221695d
Brehyma Traore <brehymatraore50@gmail.com>
2024-06-12 04:35:33 -0700

    Typo fixed

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,4 +1,4 @@
-#include "binary_tree.h"
+#include "binary_trees.h"
 
 /**
  * binary_tree_size - measures the size of a binary tree
@@ -14,5 +14,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	left_size = binary_tree_size(tree->left);
 	right_size = binary_tree_size(tree->right);
-	return (1 + left_size + right_size);
+	return (left_size + right_size + 1);
 }
