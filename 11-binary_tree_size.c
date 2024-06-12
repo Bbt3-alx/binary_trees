e1922d000f88c2bf28442a329376b610310405ff
Brehyma Traore <brehymatraore50@gmail.com>
2024-06-12 04:45:13 -0700

    Updated:

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,7 +3,8 @@
 /**
  * binary_tree_size - measures the size of a binary tree
  * @tree: A pointer to the root node of the tree to measur the size
- * Return: The size or 0 if tree is NULL
+ * Return: The size
+ * or 0 if tree is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
